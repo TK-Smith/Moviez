9c7f9ea605136defabcc826393dbb41139a405eb
TK-Smith <deborah1488ali@gmail.com>
2026-02-15 17:42:46 +0100

    adding new components

---
+++ src/components/FamilyCard.jsx
@@ -0,0 +1,52 @@
+import Spinner from "./Spinner";
+import poster from "../assets/poster.png";
+import "../styles/romanceCard.css";
+import { Link } from "react-router-dom";
+
+const FamilyCard = ({ isLoading, errorMessage, familyMovies }) => {
+  var movieName = "Family";
+  return (
+    <section>
+      <h1 className="text-amber-50" style={{ fontSize: "30px" }}>
+        {movieName} <span className="text-gradient">Movies</span>
+      </h1>
+      <div>
+        {isLoading ? (
+          <Spinner />
+        ) : errorMessage ? (
+          <p className="text-red-500">{errorMessage}</p>
+        ) : (
+          <ul className="romance-movies">
+            {familyMovies.slice(0, 4).map((movie) => (
+              <div className="romance-card">
+                <a
+                  href={`https://www.youtube.com/results?search_query=${movie.title}`}
+                  target="_blank"
+                >
+                  <div className="movieImg">
+                    <img
+                      title={movie.title}
+                      src={
+                        movie.poster_path
+                          ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                          : poster
+                      }
+                      alt={movie.title}
+                    />
+                  </div>
+                </a>
+              </div>
+            ))}
+            <div className="romance-card more">
+              <Link to="/more/10751" className="cover">
+                <span className="text-gradient">More {movieName} &#10132;</span>
+              </Link>
+            </div>
+          </ul>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default FamilyCard;
