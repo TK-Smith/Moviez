d73d8e4ed12e8f66cea9973b865a5a9592a88444
TK-Smith <deborah1488ali@gmail.com>
2026-02-24 14:40:18 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import More from "./blog/More";
 import Home from "./Home";
+import Pages from "./Pages";
+import Genres from "./components/Genres";
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/more/:id" element={<More />} />
-        </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/genres" element={<Genres />} />
+        <Route path="/pages/:page" element={<Pages />} />
+        <Route path="/more/:id" element={<More />} />
+      </Routes>
 
-        <Footer />
-      </Router>
-    </>
+      <Footer />
+    </Router>
   );
 };
 
 export default App;
+// https://www.youtube.com/watch?v=LcCSy7qyflE&list=RDLcCSy7qyflE&start_radio=1
